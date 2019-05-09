2022916d2f107caa375c5ff3805af6fde6fabe12
Scott Thomas <smthom05@gmail.com>
2019-05-09 17:01:12 -0600

    Add tests for getting recipes from Edamam API service, Add recipes router to app.js, Create recipes.js controller, Create logic to get recipes and format them
    Co-authored-by: Manoj Panta <34468256+manojpanta@users.noreply.github.com>

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var recipesRouter = require('./routes/api/v1/recipes');
 
 var app = express();
 
@@ -14,5 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/v1/recipes', recipesRouter);
 
 module.exports = app;
---
+++ spec/recipes.spec.js
@@ -0,0 +1,28 @@
+var shell= require('shelljs');
+var request = require('supertest');
+var app = require('../app');
+var pry = require('pryjs')
+
+describe('Edamam Recipes API', () => {
+  beforeAll(() => {
+    shell.exec('npx sequelize db:drop')
+    shell.exec('npx sequelize db:create')
+    shell.exec('npx sequelize db:migrate')
+    shell.exec('npx sequelize db:seed:all')
+  });
+
+
+  describe('Test GET /api/v1/recipes', () => {
+    test('should return a 200 status and all recipes within the desired search parameters', () => {
+      return request(app).get('/api/v1/recipes').then(response => {
+        expect(response.status).toBe(200)
+        expect(response.body).toBeInstanceOf(Object)
+        expect(Object.keys(response.body.recipe1)).toContain('cook_time')
+        expect(Object.keys(response.body.recipe1)).toContain('calories')
+        expect(Object.keys(response.body.recipe1)).toContain('image')
+        expect(Object.keys(response.body.recipe1)).toContain('label')
+        expect(Object.keys(response.body.recipe1)).toContain('ingredients')
+      })
+    })
+  });
+});
